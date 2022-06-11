d5adc6f0e3f1894f9487c9c02efc8766f2407021
akram-kamil <akrambean@gmail.com>
2022-06-11 08:59:09 +0200

    first task

---
+++ proj.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$txt ="hello world";
+$sh="k";
+$txt=$sh;
+
+echo $txt."<h1>this is my project</h1>";
+
+$_vare ="hello world"
+$__vare ="hello world"
+$2vare ="hello world"
+$_3vare ="hello world"
+$vare ="hello world"
+$var22 ="hello world"
+$varObE ="hello world"
+
+
+?>
+</body>
+</html>
